bc1411967990abc8c9050227898ed24ddb1ef909
bohdan <bshevyrov@gmail.com>
2021-08-23 03:06:20 +0300

    Initial commit

---
+++ src/main/java/com/example/CleanVideoLink/CleanVideoLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.CleanVideoLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanVideoLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanVideoLinkApplication.class, args);
+	}
+
+}
